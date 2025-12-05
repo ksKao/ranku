f9a9041435e110254e2978acdcd3f5d0c6f51afa
Kai Siang Kao <work@ks-kao.com>
2025-12-05 14:42:56 +0800

    Init commit

---
+++ cmd/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	log.Println("Server listening on 3000")
+	http.ListenAndServe(":3000", r)
+}
