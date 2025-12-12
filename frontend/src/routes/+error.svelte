5ae1b68feb102a6279a7b4257c5c18b49b72159d
Kai Siang Kao <work@ks-kao.com>
2025-12-12 23:00:24 +0800

    Add error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import * as Empty from '$lib/components/ui/empty/index';
+	import { page } from '$app/state';
+	import { BanIcon, GlobeIcon } from 'lucide-svelte';
+</script>
+
+<div class="flex h-full w-full items-center justify-center">
+	<Empty.Root>
+		<Empty.Header>
+			{#if page.status === 404}
+				<Empty.Media variant="icon">
+					<GlobeIcon />
+				</Empty.Media>
+				<Empty.Title class="text-xl">Oops!</Empty.Title>
+				<Empty.Description>We can't seem to find the page you're looking for.</Empty.Description>
+			{:else}
+				<Empty.Media variant="icon">
+					<BanIcon />
+				</Empty.Media>
+				<Empty.Title class="text-xl">Oops!</Empty.Title>
+				<Empty.Description>An unexpected error occured.</Empty.Description>
+			{/if}
+		</Empty.Header>
+	</Empty.Root>
+</div>
