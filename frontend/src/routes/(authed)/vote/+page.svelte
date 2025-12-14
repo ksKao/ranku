8e00d78b1c7e6a7dee20897f7b86349a89eccbfc
Kai Siang Kao <work@ks-kao.com>
2025-12-14 17:31:47 +0800

    Implement voting frontend logic and ui

--- frontend/src/routes/(authed)/vote/+page.svelte
+++ frontend/src/routes/(authed)/vote/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import * as Alert from '$lib/components/ui/alert/index';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { kyClientAuthed } from '$lib/ky';
+	import { kyClient, kyClientAuthed } from '$lib/ky';
 	import { matchupSchema } from '$lib/schemas/matchup.schema';
-	import { createQuery } from '@tanstack/svelte-query';
+	import { createMutation, createQuery } from '@tanstack/svelte-query';
 	import { CircleAlertIcon, LoaderCircleIcon, RefreshCcwIcon } from 'lucide-svelte';
 	import VoteCharacterCard from './vote-character-card.svelte';
+	import { toast } from 'svelte-sonner';
 
 	const query = createQuery(() => ({
 		queryKey: ['getVoteMatchup'],
@@ -18,6 +19,32 @@
 
 		refetchInterval: false
 	}));
+
+	const createVoteMutation = createMutation(() => ({
+		mutationFn: async ({
+			forCharacterId,
+			againstCharacterId
+		}: {
+			forCharacterId: string;
+			againstCharacterId: string;
+		}) => {
+			await kyClientAuthed.post('votes', {
+				json: {
+					forCharacterId,
+					againstCharacterId
+				}
+			});
+		},
+		onSuccess: () => {
+			query.refetch();
+		},
+		onError: () => {
+			toast.error('Failed to vote for character. Please try again later.');
+		}
+	}));
+
+	const char1 = $derived(query.data?.char1);
+	const char2 = $derived(query.data?.char2);
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
@@ -28,7 +55,7 @@
 			<CircleAlertIcon />
 			<Alert.Title>Unable to retrieve vote matchup at this time.</Alert.Title>
 		</Alert.Root>
-	{:else if !query.data.char1 || !query.data.char2}
+	{:else if !char1 || !char2}
 		<Alert.Root variant="default" class="w-fit">
 			<CircleAlertIcon />
 			<Alert.Title>
@@ -38,15 +65,36 @@
 		</Alert.Root>
 	{:else}
 		<div class="flex flex-col items-center gap-8">
-			<div class="grid grid-cols-1 md:grid-cols-3">
-				{#key query.data.char1.id}
-					<VoteCharacterCard character={query.data.char1} />
+			<div class="grid grid-flow-col grid-rows-[auto_auto_auto_auto] place-items-center gap-4">
+				{#key char1.id}
+					<VoteCharacterCard
+						character={char1}
+						loading={createVoteMutation.isPending}
+						onVote={() => {
+							createVoteMutation.mutate({
+								forCharacterId: char1.id,
+								againstCharacterId: char2.id
+							});
+						}}
+					/>
 				{/key}
-				<div class="my-4 flex items-center justify-center">
+				<div></div>
+				<div class="my-4 flex items-center justify-center md:px-8">
 					<span class="text-2xl font-bold">VS</span>
 				</div>
-				{#key query.data.char2.id}
-					<VoteCharacterCard character={query.data.char2} />
+				<div></div>
+				<div></div>
+				{#key char2}
+					<VoteCharacterCard
+						character={char2}
+						loading={createVoteMutation.isPending}
+						onVote={() => {
+							createVoteMutation.mutate({
+								forCharacterId: char2.id,
+								againstCharacterId: char1.id
+							});
+						}}
+					/>
 				{/key}
 			</div>
 			<Button variant="secondary" class="w-fit" onclick={() => query.refetch()}>
--- frontend/src/routes/(authed)/vote/vote-character-card.svelte
+++ frontend/src/routes/(authed)/vote/vote-character-card.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
-	import { characterSchema, type CharacterSchema } from '$lib/schemas/character.schema';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { type CharacterSchema } from '$lib/schemas/character.schema';
 	import { ImageOffIcon } from 'lucide-svelte';
 
 	type Props = {
 		character: CharacterSchema;
+		loading: boolean;
+		onVote: () => void;
 	};
 
 	const props: Props = $props();
 </script>
 
-<div class="flex flex-col items-center gap-2 text-lg font-bold">
-	<p>{props.character.name}</p>
-	<div class="relative h-[200px] w-[200px] overflow-hidden rounded-md">
-		{#if props.character.image.endsWith('default.jpg')}
-			<div class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-muted">
-				<ImageOffIcon class="text-muted-foreground" />
-			</div>
-		{:else}
-			<img
-				src={props.character.image}
-				alt={`${props.character.name} Image`}
-				class="absolute top-0 left-0 w-full"
-			/>
-		{/if}
-	</div>
-	<p class="max-w-[200px] text-center text-muted-foreground">{props.character.anime}</p>
+<p class="text-center">{props.character.name}</p>
+<div class="relative h-[150px] w-[150px] overflow-hidden rounded-md md:h-[200px] md:w-[200px]">
+	{#if props.character.image.endsWith('default.jpg')}
+		<div class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-muted">
+			<ImageOffIcon class="text-muted-foreground" />
+		</div>
+	{:else}
+		<img
+			src={props.character.image}
+			alt={`${props.character.name} Image`}
+			class="absolute top-0 left-0 w-full"
+		/>
+	{/if}
 </div>
+<p class="max-w-[200px] text-center text-muted-foreground">{props.character.anime}</p>
+<Button onclick={props.onVote}>Vote</Button>
